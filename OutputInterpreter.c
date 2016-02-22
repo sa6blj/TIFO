1fc88314cd1dd4682284d5497b36407e1389fe52
Jesper Ivarsson <sa6blj@gmail.com>
2016-02-22 14:57:09 +0100

    Added some init functions

--- OutputInterpreter.c
+++ OutputInterpreter.c
@@ -8,6 +8,32 @@
 
 #define LEDS_OUT <insert memory adress here>
 
+// Init port for read and write
+SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
+SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
+
+// Init Port A and B as outputs
+GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1 | 
+									   GPIO_PIN_2 | GPIO_PIN_3 | 
+									   GPIO_PIN_4 | GPIO_PIN_5 | 
+									   GPIO_PIN_6 | GPIO_PIN_7);
+
+GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | 
+									   GPIO_PIN_2 | GPIO_PIN_3 | 
+									   GPIO_PIN_4 | GPIO_PIN_5 | 
+									   GPIO_PIN_6 | GPIO_PIN_7);
+
 void updateOutputs(int col) {
-	// gpioPinWrite(LEDS_OUT, col);
+	/*
+	 * void GPIOPinWrite (uint32_t ui32Port, uint)
+	 *
+	 *
+	 */
+GPIOPinWrite(GPIO_PORTA_BASE,
+(GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_3 |
+GPIO_PIN_4 | GPIO_PIN_5),
+(GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 |
+GPIO_PIN_7));
+
+
 }
