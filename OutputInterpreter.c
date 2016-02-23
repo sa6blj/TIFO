098895c2f81aa49ec1d7a32d1fa86782fb96544f
Jesper Ivarsson <sa6blj@gmail.com>
2016-02-24 00:45:35 +0100

    Moved some stuff around. Also commented code

--- OutputInterpreter.c
+++ OutputInterpreter.c
@@ -6,7 +6,7 @@
  
 #include "OutputInterpreter.h"
 
-#define LEDS_OUT <insert memory adress here>
+/* I threw this part into main.c, revert if I made something stupid :) /J
 
 // Init port for read and write
 SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
@@ -16,12 +16,14 @@ SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOB);
 GPIOPinTypeGPIOOutput(GPIO_PORTA_BASE, GPIO_PIN_0 | GPIO_PIN_1 | 
 									   GPIO_PIN_2 | GPIO_PIN_3 | 
 									   GPIO_PIN_4 | GPIO_PIN_5 | 
-									   GPIO_PIN_6 | GPIO_PIN_7);
+									   GPIO_PIN_6 | GPIO_PIN_7i);
 
 GPIOPinTypeGPIOOutput(GPIO_PORTB_BASE, GPIO_PIN_0 | GPIO_PIN_1 | 
 									   GPIO_PIN_2 | GPIO_PIN_3 | 
 									   GPIO_PIN_4 | GPIO_PIN_5 | 
 									   GPIO_PIN_6 | GPIO_PIN_7);
+*/
+
 
 void updateOutputs(int col) {
 	/*
@@ -29,11 +31,7 @@ void updateOutputs(int col) {
 	 *
 	 *
 	 */
-GPIOPinWrite(GPIO_PORTA_BASE,
-(GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_3 |
-GPIO_PIN_4 | GPIO_PIN_5),
-(GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 |
-GPIO_PIN_7));
-
+	GPIOPinWrite(GPIO_PORTA_BASE,(GPIO_PIN_0 | GPIO_PIN_2 | GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5),
+				(GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6 | GPIO_PIN_7));
 
 }
