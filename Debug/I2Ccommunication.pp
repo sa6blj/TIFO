d0e81876e0854718896ab388898bfd2894d10324
Patrik Haar <haar@student.chalmers.se>
2016-02-26 14:19:37 +0100

    Fixed merge and gitignore

--- Debug/I2Ccommunication.pp
+++
@@ -1,5 +0,0 @@
-# FIXED
-
-I2Ccommunication.obj: ../I2Ccommunication.c
-
-../I2Ccommunication.c: 
--- Debug/OutputInterpreter.pp
+++
@@ -1,7 +0,0 @@
-# FIXED
-
-OutputInterpreter.obj: ../OutputInterpreter.c
-OutputInterpreter.obj: ../OutputInterpreter.h
-
-../OutputInterpreter.c: 
-../OutputInterpreter.h: 
