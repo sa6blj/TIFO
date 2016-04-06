6cdbfb85a54f7ec07d59dde474dd56cf8d26b0aa
Patrik Haar <haar@student.chalmers.se>
2016-04-06 15:37:19 +0200

    Removed unecessay line

--- MPU_9150.c
+++ MPU_9150.c
@@ -31,7 +31,6 @@
 #define PWR_MGMT_2 0x6c
 
 void initAccelerometer() {
-	I2CReceiveShort(BASE_ADDRESS, PWR_MGMT_1);
 	I2CSend(BASE_ADDRESS, PWR_MGMT_1, 0x80);	//Reset the unit
 	SysCtlDelay(200000);
 	I2CSend(BASE_ADDRESS, PWR_MGMT_1, 0x09);	//Disable thermometer and use the gyro as clock source
